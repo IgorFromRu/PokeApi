16577936ba37c8215252b7a85821c68832e08dab
Igor Rom <yyy1407198888@gmail.com>
2021-07-26 23:14:28 +0400

    splash screen

---
+++ app/src/main/java/com/bingerdranch/android/pokeapi/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.bingerdranch.android.pokeapi
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
+
